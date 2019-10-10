524461d8355671a17603cfae3cc42a686f829f87
Cakmakcioglu <cakmakcioglu1@gmail.com>
2019-10-11 00:20:46 +0300

    Scroll background just script added, not done yet

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    float speed = .2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        
+    }
+    //Arka planı kaydırmak için kullanacağım ama daha başaramadım.
+}
