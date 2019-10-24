ba90bd3e9cca4be5fe4a08ae70954d3660dc0178
meliksahince <meliksahince@gmail.com>
2019-10-24 22:33:28 +0300

    AudioManager and BestScore system Created
    AudioManager has been created and i defined a sample audio and call it from enemy trigger to play when enemy destroy.
    
    BestScore system errors has been fixed.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreText;
+
+    public Text bestScoreText;
     
    
     int score = 0;
     // Start is called before the first frame update
     void Start()
     {
+       GetBestScore();
+    }
 
+    
+     void GetBestScore(){
+        bestScoreText.text = PlayerPrefs.GetInt("BestScore" , 0).ToString();
     }
 
     public void addScore(int point){
         score += point;
         scoreText.text = score.ToString();
+
+         if (score > PlayerPrefs.GetInt("BestScore", 0))
+        {
+            bestScoreText.text = score.ToString();
+            PlayerPrefs.SetInt("BestScore",score);
+        }
     }
 }
